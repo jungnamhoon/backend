c08b1fd6534eb4743825d77837d79959b81697ea
Kim NamHoon <southernlight3816@gmail.com>
2025-09-27 21:20:37 +0900

    fix : SecurityConfig requestMatcher 스웨거 제외 오류 수정

--- src/main/java/com/hkorea/skyisthelimit/common/security/config/SecurityConfig.java
+++ src/main/java/com/hkorea/skyisthelimit/common/security/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
 
     http
         .authorizeHttpRequests((auth) -> auth
-            .requestMatchers("/v3/api-docs/**", "swagger-ui.html", "/swagger-ui/**").permitAll()
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
             .anyRequest().authenticated());
 
     http
