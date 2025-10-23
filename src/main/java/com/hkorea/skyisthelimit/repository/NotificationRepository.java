0613dd0ab2a6705fd4a4484f72a8d021cfab0262
Kim NamHoon <southernlight3816@gmail.com>
2025-10-23 13:17:44 +0900

    feat : 자정마다 알림 삭제하는 기능 추가(오늘로부터 7일, 읽은 것만 삭제)

--- src/main/java/com/hkorea/skyisthelimit/repository/NotificationRepository.java
+++ src/main/java/com/hkorea/skyisthelimit/repository/NotificationRepository.java
@@ -2,10 +2,13 @@ package com.hkorea.skyisthelimit.repository;
 
 import com.hkorea.skyisthelimit.entity.Member;
 import com.hkorea.skyisthelimit.entity.Notification;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
   List<Notification> findByMemberOrderByCreatedAtDesc(Member member);
+
+  List<Notification> findAllByIsReadTrueAndCreatedAtBefore(LocalDateTime dateTime);
 }
